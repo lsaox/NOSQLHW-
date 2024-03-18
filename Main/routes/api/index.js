c97f5a7bea6a0d2f6db49cedc89cf45c4643791f
unknown <el_word9@yahoo.com>
2024-03-17 23:54:09 -0500

    edited added diff start code and edited date format js

--- Main/routes/api/index.js
+++ Main/routes/api/index.js
@@ -3,5 +3,4 @@ const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
-
 module.exports = router;
